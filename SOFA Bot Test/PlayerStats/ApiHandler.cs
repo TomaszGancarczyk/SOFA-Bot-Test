b493312df3cc9627927a2a261681c9f0398f5b9c
Tomasz Gancarczyk <tom.g@onet.pl>
2025-04-28 23:10:31 +0200

    Changed api calls to demo

--- SOFA Bot Test/PlayerStats/ApiHandler.cs
+++ SOFA Bot Test/PlayerStats/ApiHandler.cs
@@ -15,15 +15,24 @@ namespace SOFA_Bot_Test.PlayerStats
             string apitoken = BotInfo.GetApiToken();
 
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://dapi.stalcraft.net/eu/character/by-name/");
+            //client.BaseAddress = new Uri("https://dapi.stalcraft.net/EU/character/by-name/");
+            client.BaseAddress = new Uri("https://dapi.stalcraft.net/EU/characters");
             client.DefaultRequestHeaders.Authorization =
                 new AuthenticationHeaderValue("Bearer", apitoken);
             client.DefaultRequestHeaders.Accept
                 .Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage response = client.GetAsync($"{playerName}/profile").Result;
-            //returns 404
-            Console.WriteLine("e");
-            return null;
+            //HttpResponseMessage response = client.GetAsync($"{playerName}/profile").Result;
+            HttpResponseMessage response = client.GetAsync($"").Result;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                logger.LogInformation("{Time} - Successfully got the request for {player}", DateTime.Now, playerName);
+                return "e";
+            }
+            else
+            {
+                logger.LogWarning("{Time} - Couldnt find player {player}", DateTime.Now, playerName);
+                return null;
+            }
         }
     }
 }
